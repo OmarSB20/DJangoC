c4d6f48c42daf9727dc82d15473b4f7acce614a6
OmarSB20 <alexsuarez00@gmail.com>
2024-01-07 19:02:59 -0600

    templates commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hellow),
+    path('', views.index),
     path('about/', views.about),
+    path('hellow/<str:usrname>', views.hellow),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Tasks
 
 # Create your views here.
-def hellow(request):
-    return HttpResponse("<h1>Hellow World!<h1>")
+def index(request):
+    title = "Welcome to Django Course"
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return HttpResponse("About")
+    usrname = "Omar"
+    return render(request, 'about.html', {
+        'usrname' : usrname
+    })
+
+def hellow(request, usrname):
+    print(usrname)
+    return HttpResponse("<h1>Hellow %s<h1>" % usrname)
+
+def projects(request):
+    #projects_list = list(Project.objects.values())
+    projects_list = Project.objects.all()
+    return render(request,"projects.html", {
+        "projects": projects_list
+    })
+
+def tasks(request):
+    tasks_list = Tasks.objects.all()
+    return render(request,"tasks.html", {
+        "tasks": tasks_list
+    })
